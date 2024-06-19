f258d071c4d2e97b886631e220eab0bfa2172b3e
RoNotBroYT <ronotbro@gmail.com>
2024-06-19 21:18:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document is ready');
+
+    // Subscribe button functionality
+    const subscribeBtn = document.getElementById('subscribeBtn');
+    subscribeBtn.addEventListener('click', function() {
+        window.location.href = 'http://www.youtube.com/@RoNotBroYT';
+    });
+});
